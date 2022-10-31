5b93914051bef7066a462e0221b9711622ac1685
haisha <hyaloids@163.com>
2022-10-31 11:38:35 +0800

    add xfer.sh

---
+++ xfer.sh
@@ -0,0 +1,10 @@
+mkdir bc_test && cd bc_test
+cp ../test/* .
+test_files=$(ls ../test/)
+for file in $test_files; do
+    prefix=${file:0:6}
+    bc_file="$prefix.bc"
+    clang -emit-llvm -c -O0 -g3 $file -o $bc_file
+    llvm-dis $bc_file
+done
+rm ./*.c
